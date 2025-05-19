d6b54aba93805b77bfc3b0ed5ebd227e522a5301
Gennaro Raiola <gennaro.raiola@gmail.com>
2025-05-19 12:43:36 +0200

    Update satate machine to use the new getters

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -182,7 +182,7 @@ void ControllerStandingUpState::updateStateMachine(StateMachine* state_machine, 
   controller->updateBaseReferences(pos_, vel_, R_);
   if (!controller->updateSolver(controller->des_joint_positions_)) {
     state_machine->setCurrentState(StateMachine::ANOMALY);
-  } else if (controller->getStateEstimator()->getEstimatedBaseHeight() >= controller->robot_model_->getStandUpHeight()) {
+  } else if (controller->getStateEstimator()->getBaseHeightInBasefoot() >= controller->robot_model_->getStandUpHeight()) {
     state_machine->setCurrentState(StateMachine::ACTIVE);
   }
 }
